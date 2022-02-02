20df7ec6734ccfe79bef170335f226549f1cf2d9
Leonardo <leonardo123k@gmail.com>
2022-02-02 14:10:07 -0300

    update eslint config to work with vscode and add jest config

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 // Ativa o uso das variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const server = express();
 
-server.use('/', (request, response) => {
+server.use("/", (request, response) => {
   const { name } = request.query;
   return response.json(`Hello, ${name}`);
 });
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server running at port ${process.env.PORT}`)
-})
+  console.log(`Server running at port ${process.env.PORT}`);
+});
